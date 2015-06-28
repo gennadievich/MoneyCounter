a0e8efec3a3f262bc0a2f5371493adf2e9592b44
Kirill <ay@mail.by>
2015-06-28 18:45:35 +0300

    develop in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      redirect_to user_path(@user)
+    else
+      render new
+    end
+
   end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
