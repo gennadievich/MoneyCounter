5e2485d9c8864c37f5b6524d834961927384a7eb
Kirill <ay@mail.by>
2015-08-05 22:25:53 +0300

    users page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
     $('#chng-btn-link').click(function(e){
         $('.chng-pass-div').fadeIn('slow');
         $('#chng-btn-link').hide();
+        $('.user-info').hide();
         $('#cancel-btn-link').toggle();
         e.preventDefault();
     });
 
     $('#cancel-btn-link').click(function(e){
-        $('.chng-pass-div').fadeOut('fast');
+        $('.chng-pass-div').hide();
         $('#cancel-btn-link').hide();
+        $('.user-info').fadeIn('slow');
         $('#chng-btn-link').toggle();
         e.preventDefault();
     });
